63eae25bfa26ff669772a713080641368fd37dcf
Alex <36400696+skillfi@users.noreply.github.com>
2024-11-23 18:39:45 +0200

    Fixes

--- src/main/java/com/github/skillfi/reincarnation_plus/config/package-info.java
+++
@@ -1,8 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-
-package com.github.skillfi.reincarnation_plus.config;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
